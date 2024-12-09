3ba0b60fca49f77cc2a6e3a871cb018b5c989b4c
Dadatoa <f0a202@outlook.com>
2024-12-09 17:54:44 +0700

    remove subdirs

--- home-manager/nvim/plugins/oil.lua
+++
@@ -1,5 +0,0 @@
-require('oil').setup()
-  vim.keymap.set('n', '<leader>e', require('oil').open, {})
-  vim.keymap.set('n', '<leader>E', require('oil').open_float, {})
-  vim.keymap.set('n', '-', require('oil').open, { desc = 'Open parent directory' })
-
