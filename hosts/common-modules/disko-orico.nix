db2c4ce6b007a4fcb1a9dbf31b997bf4c6a3d2d2
Dadatoa <f0a202@outlook.com>
2025-02-27 14:25:02 +0700

    create disko for backup drive

---
+++ hosts/common-modules/disko-orico.nix
@@ -0,0 +1,52 @@
+{
+  disko.devices = {
+    
+    orico1 = {
+      type = "disk";
+      device = "/dev/disk/by-id/ata-TOSHIBA_DT01ACA050_X8LDNTAKS";
+      content = {
+        type = "gpt";
+        partitions = {
+          root = {
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "orico";
+            };
+          };  
+        };
+      };
+    };
+    
+    orico2 = {
+      type = "disk";
+      device = "/dev/disk/by-id/ata-WDC_WD40PURX-64AKYY0_WD-WX92DA1J6L8Z";
+      content = {
+        type = "gpt";
+        partitions = {
+          root = {
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "orico";
+            };
+          };  
+        };
+      };
+    };
+
+    lvm_vg = {
+      type = "lvm_vg";
+      lvs = {
+        backup_vg = {
+          size = "50%FREE";
+          content = {
+            type = "btrfs";
+            extraArgs = [ "-f" ];
+          };
+        };
+      };
+    };
+
+  };
+}
