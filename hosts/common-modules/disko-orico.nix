5ebecdb49f87bb453021eb9552f8b99e00e69e35
Dadatoa <f0a202@outlook.com>
2025-03-04 10:10:23 +0700

    remove lvm

--- hosts/common-modules/disko-orico.nix
+++ hosts/common-modules/disko-orico.nix
@@ -10,8 +10,8 @@
           root = {
             size = "100%";
             content = {
-              type = "lvm_pv";
-              vg = "orico-pool";
+              type = "btrfs";
+              extraArgs = [ "-f" ];
             };
           };  
         };
@@ -27,25 +27,10 @@
           root = {
             size = "100%";
             content = {
-              type = "lvm_pv";
-              vg = "orico-pool";
+              type = "btrfs";
+              extraArgs = [ "-f" ];
             };
           };  
-        };
-      };
-    };
-    };
-
-    lvm_vg = {
-    orico-pool = {
-      type = "lvm_vg";
-      lvs = {
-        backup_lv = {
-          size = "50%FREE";
-          content = {
-            type = "btrfs";
-            extraArgs = [ "-f" ];
-          };
         };
       };
     };
