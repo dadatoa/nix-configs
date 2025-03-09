54c420daccbbacfe14e7fc4ca6dd9d4641f4b2e7
Dadatoa <f0a202@outlook.com>
2025-03-09 14:04:07 +0700

    add some config for utm virtualised testing machine

--- hosts/common-modules/configuration.nix
+++ hosts/common-modules/configuration.nix
@@ -44,16 +44,19 @@
   btrfs-progs
   curl 
   exfat 
+  fzf
   git
   hdparm
   neovim
   nmap
   overmind # process manager
   pv # progress bar when copy file
+  sesh
   tailscale
   tmux
   wezterm # to use with wezterm connect command
   wget
+  zoxyde
   ];
 
   # Enable networking config with network manager
@@ -74,6 +77,7 @@
 
     # Enable Tailscale
     tailscale.enable = true;
+    tailscale.openFirewall = true;
 
     # Avahi for auto discover based on hostname
     avahi = {
--- hosts/utm-lab-1/configuration.nix
+++ hosts/utm-lab-1/configuration.nix
@@ -1,12 +1,18 @@
-{ config, pckgs, ... }:
+{ config, pkgs, ... }:
 {
   imports = [
     ../common-modules/configuration.nix
     ../common-modules/users.nix
+    ../common-modules/virtualisation.nix
     /etc/nixos/hardware-configuration.nix
   ];
 
   networking.hostName = "utm-lab-1";
   networking.firewall.enable = false;
-  
+  environment.systemPackages = [
+  pkgs.sqlite
+  ];
+
+  services.tailscale.permitCertUid = "1000";
+
 }
