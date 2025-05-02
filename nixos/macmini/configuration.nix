e6a1489f1deb7cd3e7e51cb9da4ac23ed1938c52
Dadatoa <f0a202@outlook.com>
2025-05-02 16:41:08 +0700

    add ethernet network config

--- nixos/macmini/configuration.nix
+++ nixos/macmini/configuration.nix
@@ -10,6 +10,10 @@
   
   networking.hostName = "macmini";
   networking.firewall.enable = false;
+  networking.interfaces.enp2s0f0.ipv4.addresses = [{
+        address = "192.168.0.3";
+        prefixLength = 24;
+    }];
 
   services.samba.enable = true;
   services.samba.package = pkgs.samba4Full;
