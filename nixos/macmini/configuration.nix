0c3db10e862204436559d19fd64da1ae403b2f98
Dadatoa <f0a202@outlook.com>
2025-05-11 18:46:06 +0700

    Squash merge networking into main

--- nixos/macmini/configuration.nix
+++ nixos/macmini/configuration.nix
@@ -1,21 +1,24 @@
-{ config, pkgs, ... }:
 {
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     ../common-modules/configuration.nix
     ../common-modules/users.nix
     /etc/nixos/hardware-configuration.nix
   ];
-  
-  boot.kernelParams = [ "vga=0x317" "nomodeset" ];
-  
+
+  boot.kernelParams = ["vga=0x317" "nomodeset"];
+
   networking.hostName = "macmini";
   networking.firewall.enable = false;
-  networking.interfaces.enp2s0f0.ipv4.addresses = [{
-        address = "192.168.0.3";
-        prefixLength = 24;
-    }];
+  # networking.interfaces.enp2s0f0.ipv4.addresses = [{
+  #       address = "192.168.0.3";
+  #       prefixLength = 24;
+  #   }];
 
   services.samba.enable = true;
   services.samba.package = pkgs.samba4Full;
-  services.samba.openFirewall = true; 
+  services.samba.openFirewall = true;
 }
