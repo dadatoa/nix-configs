fb94b618eaa469504c690ae3f0086bc2c4418bbe
Dadatoa <f0a202@outlook.com>
2025-05-20 17:23:51 +0700

    systemd network on all hosts

--- nixos/nara17/configuration.nix
+++ nixos/nara17/configuration.nix
@@ -20,19 +20,43 @@
     wakeOnLan.enable = true;
   };
 
-  networking.interfaces.vlan100 = {
-    ipv4.addresses = [
-      {
-        address = "10.120.17.250";
-        prefixLength = 26;
-      }
-    ];
-  };
+  ## manage network with systemd
+  systemd.network.enable = true;
 
-  networking.vlans = {
-    vlan100 = {
-      id = 100;
-      interface = "enp2s0";
+  systemd.network = {
+    ## declare vlan
+    netdevs = {
+      "20-vlan100" = {
+        netdevConfig = {
+          Kind = "vlan";
+          Name = "vlan100";
+          Description = "LAN Access";
+        };
+        vlanConfig = {
+          Id = 100;
+        };
+      };
+    };
+    ## network interfaces
+    networks = {
+      "30-lan" = {
+        enable = true;
+        matchConfig.Name = "enp2s0";
+        networkConfig.DHCP = "ipv4";
+        ## add vlans on physical interface
+        vlan = [
+          "vlan100"
+        ];
+      };
+      ## add virtual interfaces for vlan
+      "50-vlan100" = {
+        matchConfig.Name = "vlan100";
+        address = [
+          "10.120.17.250/26"
+          # "fd42:23:42:b865::1/64"
+          # "fe80::1/64"
+        ];
+      };
     };
   };
 
