22df05b7e8291006c67fadb2c19ad5dbfea61aa9
Dadatoa <f0a202@outlook.com>
2025-06-07 10:09:09 +0700

    Squash merge printing into main

--- nixos/macmini/configuration.nix
+++ nixos/macmini/configuration.nix
@@ -12,4 +12,28 @@
   ## boot fail on mac mini without these
   boot.kernelParams = [ "vga=0x317" "nomodeset" ];
 
+  ## printing
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [ canon-capt ];
+    allowFrom = [ "all" ];
+    listenAddresses = [ "*:631" ];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    canon-capt
+  ];
+
+  services.avahi = {
+  enable = true;
+  nssmdns4 = true;
+  openFirewall = true;
+  publish = {
+    enable = true;
+    userServices = true;
+  };
+};
 }
