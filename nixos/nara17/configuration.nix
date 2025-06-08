ebdd5c10e20ef13a27e226ce2d1914911a4e9639
Dadatoa <f0a202@outlook.com>
2025-06-08 13:53:11 +0700

    Squash merge network into main

--- nixos/nara17/configuration.nix
+++ nixos/nara17/configuration.nix
@@ -1,80 +1,17 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
+{ config
+, lib
+, pkgs
+, ...
 }: {
   imports = [
     ../common-modules/configuration.nix
     ../common-modules/users.nix
     ../common-modules/virtualisation.nix
     ./storage.nix
+    ./networking.nix
     /etc/nixos/hardware-configuration.nix
   ];
 
-  networking.hostName = "nara17";
-  networking.hostId = "deadb33f";
-  networking.firewall.enable = false;
-
-  networking.interfaces.enp2s0 = {
-    wakeOnLan.enable = true;
-  };
-
-  ## disable network manager to work with wp supplicant and systemd network instead
-  networking.networkmanager.enable = false;
-
-  ## wireless-related configuration
-  networking.wireless = {
-    enable = true;
-    secretsFile = "/run/secrets/wireless.conf";
-    interfaces = [
-      "wlp0s20f0u1"
-    ];
-    networks."Natcha5G" = {
-      pskRaw = "ext:natcha17_psk";
-    };
-    ## allow user (wheel group) to interact with wpa_supplicant
-    userControlled.enable = true;
-  };
-  ## manage network with systemd
-  systemd.network.enable = true;
-
-  systemd.network = {
-    ## declare vlan
-    netdevs = {
-      "20-vlan100" = {
-        netdevConfig = {
-          Kind = "vlan";
-          Name = "vlan100";
-          Description = "LAN Access";
-        };
-        vlanConfig = {
-          Id = 100;
-        };
-      };
-    };
-    ## network interfaces
-    networks = {
-      "30-lan" = {
-        enable = true;
-        matchConfig.Name = "enp2s0";
-        networkConfig.DHCP = "ipv4";
-        ## add vlans on physical interface
-        vlan = [
-          "vlan100"
-        ];
-      };
-      ## add virtual interfaces for vlan
-      "50-vlan100" = {
-        matchConfig.Name = "vlan100";
-        address = [
-          "10.120.17.250/26"
-          # "fd42:23:42:b865::1/64"
-          # "fe80::1/64"
-        ];
-      };
-    };
-  };
 
   services.samba.enable = true;
   services.samba.package = pkgs.samba4Full;
---
+++ nixos/nara17/networking.nix
@@ -0,0 +1,60 @@
+{ config
+, lib
+, ...
+}: {
+
+  networking.hostName = "nara17";
+
+  networking.firewall.enable = false;
+  networking.interfaces.enp2s0.wakeOnLan.enable = true;
+
+  ## manage network with systemd
+  networking.useNetworkd = true;
+  systemd.network.enable = true;
+  systemd.network = {
+    ## declare vlan
+    netdevs = {
+      "20-vlan100" = {
+        netdevConfig = {
+          Kind = "vlan";
+          Name = "vlan100";
+          Description = "LAN Access";
+        };
+        vlanConfig = {
+          Id = 100;
+        };
+      };
+    };
+    ## network interfaces
+    networks = {
+      "30-lan" = {
+        enable = true;
+        matchConfig.Name = "enp2s0";
+        networkConfig.DHCP = "ipv4";
+        ## add vlans on physical interface
+        vlan = [
+          "vlan100"
+        ];
+      };
+      ## add virtual interfaces for vlan
+      "50-vlan100" = {
+        matchConfig.Name = "vlan100";
+        address = [
+          "10.120.17.250/26"
+          # "fd42:23:42:b865::1/64"
+          # "fe80::1/64"
+        ];
+      };
+    };
+  };
+
+  ## wireless
+  networking.wireless = {
+    enable = true;
+    # secretsFile = "/run/secrets/wireless.conf";
+    interfaces = [
+      "wlp0s20f0u1"
+    ];
+    # userControlled.enable = true;
+  };
+}
