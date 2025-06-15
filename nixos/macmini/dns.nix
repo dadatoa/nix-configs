736c36870ef0bba130e534181b5a5dcbca694282
Dadatoa <f0a202@outlook.com>
2025-06-15 19:09:44 +0700

    open some ports for dns

--- nixos/macmini/dns.nix
+++ nixos/macmini/dns.nix
@@ -3,5 +3,26 @@
   services.adguardhome = {
     enable = true;
     openFirewall = true;
+    settings = {
+      dns = {
+        upstream_dns = [
+          # Example config with quad9
+          "9.9.9.9#dns.quad9.net"
+          "149.112.112.112#dns.quad9.net"
+          # Uncomment the following to use a local DNS service (e.g. Unbound)
+          # Additionally replace the address & port as needed
+          # "127.0.0.1:5335"
+        ];
+      };
+      filtering = {
+        protection_enabled = true;
+        filtering_enabled = true;
+
+        parental_enabled = false; # Parental control-based DNS requests filtering.
+        safe_search = {
+          enabled = false; # Enforcing "Safe search" option for search engines, when possible.
+        };
+      };
+    };
   };
 }
--- nixos/macmini/networking.nix
+++ nixos/macmini/networking.nix
@@ -1,20 +1,31 @@
-{ config
-, lib
-, ...
-}: {
+{
+  config,
+  lib,
+  ...
+}:
+{
 
   networking.hostName = "macmini";
   networking.interfaces.enp2s0f0.wakeOnLan.enable = true;
 
-  networking.firewall.enable = true;
-  networking.firewall.extraCommands = ''
-    # Set up SNAT on packets going from downstream to the wider internet
-    iptables -t nat -A POSTROUTING -o wlp3s0 -j MASQUERADE
+  networking.firewall = {
+    enable = true;
+    extraCommands = ''
+      # Set up SNAT on packets going from downstream to the wider internet
+      iptables -t nat -A POSTROUTING -o wlp3s0 -j MASQUERADE
 
-    # Accept all connections from downstream. May not be necessary
-    iptables -A INPUT -i enp2s0f0 -j ACCEPT  
-  '';
-
+      # Accept all connections from downstream. May not be necessary
+      iptables -A INPUT -i enp2s0f0 -j ACCEPT  
+    '';
+    allowedTCPPorts = [
+      22
+      53
+      80
+      443
+      8080
+      8443
+    ];
+  };
   ## manage network with systemd
   networking.useNetworkd = true;
   systemd.network.enable = true;
