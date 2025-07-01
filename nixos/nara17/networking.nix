766af833ff094fc8fb5c39d0a8414e76db2ad96b
Dadatoa <f0a202@outlook.com>
2025-07-01 08:24:15 +0700

    dhcp

--- nixos/nara17/networking.nix
+++ nixos/nara17/networking.nix
@@ -32,10 +32,10 @@
       "30-lan" = {
         enable = true;
         matchConfig.Name = "enp2s0";
-        # networkConfig.DHCP = "ipv4";
-        address = [
-          "10.120.17.248/28"
-        ];
+        networkConfig.DHCP = "ipv4";
+        # address = [
+        #   "10.120.17.248/28"
+        # ];
         ## add vlans on physical interface
         vlan = [
           "vlan100"
@@ -44,12 +44,12 @@
       ## add virtual interfaces for vlan
       "50-vlan100" = {
         matchConfig.Name = "vlan100";
-        # networkConfig.DHCP = "ipv4";
-        address = [
-          "10.120.17.96/26"
+        networkConfig.DHCP = "ipv4";
+        # address = [
+        #   "10.120.17.96/26"
           #   # "fd42:23:42:b865::1/64"
           #   # "fe80::1/64"
-        ];
+        # ];
       };
     };
   };
