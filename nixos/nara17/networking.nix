40b50799726ca120dc0a903caea31d1a67c29610
Dadatoa <f0a202@outlook.com>
2025-07-13 16:48:47 +0700

    wol give error on nixos-rebuild

--- nixos/nara17/networking.nix
+++ nixos/nara17/networking.nix
@@ -8,7 +8,7 @@
   networking.hostName = "nara17";
 
   networking.firewall.enable = false;
-  networking.interfaces."enp2s0".wakeOnLan.enable = true;
+  # networking.interfaces."enp2s0".wakeOnLan.enable = true; # issue when rebuild?
 
   ## manage network with systemd
   networking.useNetworkd = true;
