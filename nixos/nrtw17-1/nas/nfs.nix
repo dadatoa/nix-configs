d2367a6b4b76e9e72c4d46b8f655181245fba64f
dadatoa <f0a202@outlook.com>
2025-07-26 19:07:15 +0700

    just 1 bind mount on the complete volume

--- nixos/nrtw17-1/nas/nfs.nix
+++ nixos/nrtw17-1/nas/nfs.nix
@@ -1,27 +1,24 @@
 { ... }:
 {
   ## mountpoints
-  fileSystems."/export/media" = {
-    device = "/mnt/datapool/media";
+  fileSystems."/export" = {
+    device = "/mnt/datapool";
     options = [ "bind" ];
   };
 
-  fileSystems."/export/appdata" = {
-    device = "/mnt/datapool/appdata";
-    options = [ "bind" ];
-  };
-
-  fileSystems."/export/share" = {
-    device = "/mnt/datapool/share";
-    options = [ "bind" ];
-  };
+  # fileSystems."/export/appdata" = {
+  #   device = "/mnt/datapool/appdata";
+  #   options = [ "bind" ];
+  # };
+  #
+  # fileSystems."/export/share" = {
+  #   device = "/mnt/datapool/share";
+  #   options = [ "bind" ];
+  # };
 
   ## nfs service
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
     /export 100.64.0.0/10(rw,fsid=0)
-    /export/media 100.64.0.0/10(rw,fsid=0,no_subtree_check)
-    /export/appdata 100.64.0.0/10(rw,fsid=0,no_subtree_check)
-    /export/share 100.64.0.0/10(rw,fsid=0,no_subtree_check)
   '';
 }
