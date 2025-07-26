146a8875c7d6ef9ed2d909658b518c826bc980ec
dadatoa <f0a202@outlook.com>
2025-07-26 18:58:51 +0700

    add root export

--- nixos/nrtw17-1/nas/nfs.nix
+++ nixos/nrtw17-1/nas/nfs.nix
@@ -19,6 +19,7 @@
   ## nfs service
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
+    /export 100.64.0.0/10(rw,fsid=0)
     /export/media 100.64.0.0/10(rw,fsid=0,no_subtree_check)
     /export/appdata 100.64.0.0/10(rw,fsid=0,no_subtree_check)
     /export/share 100.64.0.0/10(rw,fsid=0,no_subtree_check)
